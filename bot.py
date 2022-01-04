00cbdf59ee8c91866da217c76ce9f91f700c3432
manas1111 <59135271+manas1111@users.noreply.github.com>
2022-01-04 10:48:29 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -339,7 +339,16 @@ async def callBackButton(bot:Update, callback_query:CallbackQuery):
                         return await callback_query.answer(
                             "This request Is Completed🥳...\nCheckout in Channel😊",
                             show_alert = True
-                        )
+                        ) reply_markup = InlineKeyboardMarkup(
+                            [
+                                [
+                                    InlineKeyboardButton(
+                                        "Upload Channel",
+                                        url=f"https://t.me/+jHVn7RNv9WE4ZDk1"
+                                    )
+                                ]
+                            ]    
+                    
                     user = await bot.get_chat_member(int(channelID), callback_query.from_user.id)
                     if user.status not in ("administrator", "creator"): # If accepting, rejecting request tried to be done by neither admin nor owner
                         await callback_query.answer(
